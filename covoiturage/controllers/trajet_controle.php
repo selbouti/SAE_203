b6fdcf72057deb4f482fcd9b53f81e61181f6286
roland185 <roland.ndanteng.mbakeu@etu.univ-poitiers.fr>
2025-06-11 11:20:36 +0200

    Update trajet_controle.php

--- covoiturage/controllers/trajet_controle.php
+++ covoiturage/controllers/trajet_controle.php
@@ -2,6 +2,9 @@
 require_once(__DIR__ . '/../config/conf.php');
 require_once(__DIR__ . '/../models/trajet_model.php');
 $trajets = getTrajetsDuJour($pdo);
-
+function decrementer_places(PDO $pdo, int $id_trajet): bool {
+    $stmt = $pdo->prepare("UPDATE trajet SET nbr_place = nbr_place - 1 WHERE id = ? AND nbr_place > 0");
+    return $stmt->execute([$id_trajet]);
+}
 require_once(__DIR__ . '/../views/trajet_view.php');
 
