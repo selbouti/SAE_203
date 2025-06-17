165041a9189635b7d8668d6b9c68a14b245e591e
roland185 <roland.ndanteng.mbakeu@etu.univ-poitiers.fr>
2025-06-18 00:10:11 +0200

    Update ajouter_reservation.php

--- covoiturage/models/ajouter_reservation.php
+++ covoiturage/models/ajouter_reservation.php
@@ -1,19 +1,33 @@
 <?php
 
-function ajouter_reservation(PDO $connex, int $id_trajet, int $id_passager): bool {
+function ajouter_reservation(PDO $connex, int $id_trajet, int $id_passager) {
     $req = "INSERT INTO reservation (id_trajet, id_passager, dateReservation, statut)
-            VALUES (?, ?, NOW(), 'en_attente')";
-    $stmt = $connex->prepare($req);
-    return $stmt->execute([$id_trajet, $id_passager]);
+            VALUES (:id_trajet, :id_passager, NOW(), 'en_attente')";
+    
+    $prep = $connex->prepare($req);
+    $prep->bindValue(':id_trajet', $id_trajet, PDO::PARAM_INT);
+    $prep->bindValue(':id_passager', $id_passager, PDO::PARAM_INT);
+    
+    $result = $prep->execute();
+    $prep->closeCursor();
+    
+    return $result;
 }
 
 
 function find_trajet_by_id(PDO $pdo, int $id) {
-    $stmt = $pdo->prepare("SELECT t.*, u.nom AS conducteur_nom, u.prenom AS conducteur_prenom 
-                           FROM trajet t 
-                           JOIN utilisateur u ON t.id_conducteur = u.id 
-                           WHERE t.id = ?");
-    $stmt->execute([$id]);
-    return $stmt->fetch(PDO::FETCH_ASSOC);
+    $req = "SELECT t.*, u.nom AS conducteur_nom, u.prenom AS conducteur_prenom 
+            FROM trajet t 
+            JOIN utilisateur u ON t.id_conducteur = u.id 
+            WHERE t.id = :id";
+
+    $prep = $pdo->prepare($req);
+    $prep->bindValue(':id', $id, PDO::PARAM_INT);
+    $prep->execute();
+
+    $trajet = $prep->fetch(PDO::FETCH_ASSOC);
+    $prep->closeCursor();
+
+    return $trajet;
 }
 
